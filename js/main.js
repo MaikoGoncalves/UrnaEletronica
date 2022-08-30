b3c3842f1cec760b0cf40bd6e4ec1de63df71cf7
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2022-08-30 02:30:45 -0300

    30082022

--- js/main.js
+++ js/main.js
@@ -3,30 +3,64 @@ let valorN="";
 function ler_candidato(numeroCandidado){
 
     if(numeroCandidado == 22){
-        document.getElementById("nomeCandidato").innerHTML = " Bolsonario";
+        document.getElementById("nomeCandidato").innerHTML = "Nome : Bolsonario";
+        document.getElementById("cargo").innerHTML = "Cargo : PRESIDENTE";
         document.getElementById("fotoC").innerHTML = `<img src="Image/Bolsonario.png" />`;
     }
     if(numeroCandidado == 13){
-        document.getElementById("nomeCandidato").innerHTML = " Lula";
+        document.getElementById("nomeCandidato").innerHTML = "Nome : Lula";
+        document.getElementById("cargo").innerHTML = "Cargo : PRESIDENTE";
         document.getElementById("fotoC").innerHTML = `<img src="Image/Lula.png" />`;
     }
     if(numeroCandidado == 12){
-        document.getElementById("nomeCandidato").innerHTML = " Ciro";
+        document.getElementById("nomeCandidato").innerHTML = "Nome :  Ciro";
+        document.getElementById("cargo").innerHTML = "Cargo : PRESIDENTE";
         document.getElementById("fotoC").innerHTML = `<img src="Image/Ciro.png" />`;
         
     } 
     if(numeroCandidado == 15){
-        document.getElementById("nomeCandidato").innerHTML = " Simone";
+        document.getElementById("nomeCandidato").innerHTML = "Nome : Simone";
+        document.getElementById("cargo").innerHTML = "Cargo : PRESIDENTE";
         document.getElementById("fotoC").innerHTML = `<img src="Image/Simone.png" /> `;
     } 
-
+    if(numeroCandidado == 44){
+        document.getElementById("nomeCandidato").innerHTML = "Nome : Soraya";
+        document.getElementById("cargo").innerHTML = "Cargo : PRESIDENTE";
+        document.getElementById("fotoC").innerHTML = `<img src="Image/Soraya.png" /> `;
+    }
+    if(numeroCandidado == 90){
+        document.getElementById("nomeCandidato").innerHTML = "Nome : Marcal";
+        document.getElementById("cargo").innerHTML = "Cargo : PRESIDENTE";
+        document.getElementById("fotoC").innerHTML = `<img src="Image/Marcal.png" /> `;
+    }
 }
 
+function confirma() {
+    var valor1 = parseInt(document.getElementById("campo1").value);
+    var valor2 = parseInt(document.getElementById("campo2").value);
+    var candidado = (valor1 * 10) + valor2;
+   
+    if (sessionStorage.getItem(candidado) !== null) {
+        votos = parseInt(sessionStorage.getItem(candidado)) + 1;
+        sessionStorage.setItem(candidado, votos);
+    } else {
+        sessionStorage.setItem(candidado, 1);
+        
+    }
+    alert("Confirmado voto no candidato " + candidado);
+    document.getElementById("campo1").value = "";
+    document.getElementById("campo2").value = "";
+    document.getElementById("nomeCandidato").innerHTML = "";
+    document.getElementById("cargo").innerHTML = "";
+    document.getElementById("fotoC").innerHTML = "";
+    valorN='';
+    
+}
 
 function clicou(valor) {
     var valor1 = document.getElementById("campo1").value;
     var valor2 = document.getElementById("campo2").value;
-       
+        
     if (valor1 == "") {
         document.getElementById("campo1").value = valor;
         valorN= valorN + valor;
@@ -37,27 +71,23 @@ function clicou(valor) {
         
     }
    ler_candidato(valorN);
-
 }
-
-
 
 function branco() {
     alert("clicou em BRANCO")
     //document.getElementById("resultado").innerHTML="Bolsonario"
 }
 
-
-
 function corrige() {
     document.getElementById("campo1").value = "";
     document.getElementById("campo2").value = "";
     document.getElementById("nomeCandidato").innerHTML = "";
     document.getElementById("fotoC").innerHTML = "";
+    document.getElementById("cargo").innerHTML = "";
     valorN='';
 }
 
-function confirma() {
+function confirma1() {
     var valor1 = parseInt(document.getElementById("campo1").value);
     var valor2 = parseInt(document.getElementById("campo2").value);
     var candidado = (valor1 * 10) + valor2;
@@ -77,13 +107,12 @@ function confirma() {
     
 }
 
-
 function resultado() {
-    document.getElementById("resultado").innerHTML=""
+    document.getElementById("resultado1").innerHTML=""
     for(i=0;i<100;i++){
         if (sessionStorage.getItem(i) !== null) {
             //alert(i);
-            document.getElementById("resultado").innerHTML += "Cantidado "+i+" tem "+sessionStorage.getItem(i)+" votos<br/>";
+            document.getElementById("resultado1").innerHTML += "Candidado "+i+" tem: "+sessionStorage.getItem(i)+" votos<br/>";
         }
     }
 }
